25587e39dbe00bbc0f80fd57b33a7750fee0ed01
sajaniweerasekara <weerasekara.sajani@gmail.com>
2018-09-23 18:16:35 +1000

    Modify Hotel.java

--- entities/Hotel.java
+++ entities/Hotel.java
@@ -8,93 +8,90 @@ import hotel.credit.CreditCard;
 import hotel.utils.IOUtils;
 
 public class Hotel {
-	
-	private Map<Integer, Guest> guests;
-	public Map<RoomType, Map<Integer,Room>> roomsByType;
-	public Map<Long, Booking> bookingsByConfirmationNumber;
-	public Map<Integer, Booking> activeBookingsByRoomId;
-	
-	
-	public Hotel() {
-		guests = new HashMap<>();
-		roomsByType = new HashMap<>();
-		for (RoomType rt : RoomType.values()) {
-			Map<Integer, Room> rooms = new HashMap<>();
-			roomsByType.put(rt, rooms);
-		}
-		bookingsByConfirmationNumber = new HashMap<>();
-		activeBookingsByRoomId = new HashMap<>();
-	}
 
+    private Map<Integer, Guest> guests;
+    public Map<RoomType, Map<Integer, Room>> roomsByType;
+    public Map<Long, Booking> bookingsByConfirmationNumber;
+    public Map<Integer, Booking> activeBookingsByRoomId;
+    
+    long tempConfNumber;
+
+    private Booking booking;
+    int crooonfirmmid = 0;
+
+    public Hotel() {
+        guests = new HashMap<>();
+        roomsByType = new HashMap<>();
+        for (RoomType rt : RoomType.values()) {
+            Map<Integer, Room> rooms = new HashMap<>();
+            roomsByType.put(rt, rooms);
+        }
+        bookingsByConfirmationNumber = new HashMap<>();
+        activeBookingsByRoomId = new HashMap<>();
+    }
+
+    public void addRoom(RoomType roomType, int id) {
+        IOUtils.trace("Hotel: addRoom");
+        for (Map<Integer, Room> rooms : roomsByType.values()) {
+            if (rooms.containsKey(id)) {
+                throw new RuntimeException("Hotel: addRoom : room number already exists");
+            }
+        }
+        Map<Integer, Room> rooms = roomsByType.get(roomType);
+        Room room = new Room(id, roomType);
+        rooms.put(id, room);
+    }
+
+    public boolean isRegistered(int phoneNumber) {
+        return guests.containsKey(phoneNumber);
+    }
+
+    public Guest registerGuest(String name, String address, int phoneNumber) {
+        if (guests.containsKey(phoneNumber)) {
+            throw new RuntimeException("Phone number already registered");
+        }
+        Guest guest = new Guest(name, address, phoneNumber);
+        guests.put(phoneNumber, guest);
+        return guest;
+    }
+
+    public Guest findGuestByPhoneNumber(int phoneNumber) {
+        Guest guest = guests.get(phoneNumber);
+        return guest;
+    }
+
+    public Booking findActiveBookingByRoomId(int roomId) {
+        Booking booking = activeBookingsByRoomId.get(roomId);
+        return booking;
+    }
+
+    public Room findAvailableRoom(RoomType selectedRoomType, Date arrivalDate, int stayLength) {
+        IOUtils.trace("Hotel: checkRoomAvailability");
+        Map<Integer, Room> rooms = roomsByType.get(selectedRoomType);
+        for (Room room : rooms.values()) {
+            IOUtils.trace(String.format("Hotel: checking room: %d", room.getId()));
+            if (room.isAvailable(arrivalDate, stayLength)) {
+                return room;
+            }
+        }
+        return null;
+    }
+
+    public Booking findBookingByConfirmationNumber(long confirmationNumber) {
+        return bookingsByConfirmationNumber.get(confirmationNumber);
+    }
+
+    public long book(Room room, Guest guest, Date arrivalDate, int stayLength, int occupantNumber, CreditCard creditCard) {
 	
-	public void addRoom(RoomType roomType, int id) {
-		IOUtils.trace("Hotel: addRoom");
-		for (Map<Integer, Room> rooms : roomsByType.values()) {
-			if (rooms.containsKey(id)) {
-				throw new RuntimeException("Hotel: addRoom : room number already exists");
-			}
-		}
-		Map<Integer, Room> rooms = roomsByType.get(roomType);
-		Room room = new Room(id, roomType);
-		rooms.put(id, room);
-	}
-
-	
-	public boolean isRegistered(int phoneNumber) {
-		return guests.containsKey(phoneNumber);
-	}
-
-	
-	public Guest registerGuest(String name, String address, int phoneNumber) {
-		if (guests.containsKey(phoneNumber)) {
-			throw new RuntimeException("Phone number already registered");
-		}
-		Guest guest = new Guest(name, address, phoneNumber);
-		guests.put(phoneNumber, guest);		
-		return guest;
-	}
-
-	
-	public Guest findGuestByPhoneNumber(int phoneNumber) {
-		Guest guest = guests.get(phoneNumber);
-		return guest;
-	}
-
-	
-	public Booking findActiveBookingByRoomId(int roomId) {
-		Booking booking = activeBookingsByRoomId.get(roomId);;
-		return booking;
-	}
-
-
-	public Room findAvailableRoom(RoomType selectedRoomType, Date arrivalDate, int stayLength) {
-		IOUtils.trace("Hotel: checkRoomAvailability");
-		Map<Integer, Room> rooms = roomsByType.get(selectedRoomType);
-		for (Room room : rooms.values()) {
-			IOUtils.trace(String.format("Hotel: checking room: %d",room.getId()));
-			if (room.isAvailable(arrivalDate, stayLength)) {
-				return room;
-			}			
-		}
-		return null;
-	}
-
-	
-	public Booking findBookingByConfirmationNumber(long confirmationNumber) {
-		return bookingsByConfirmationNumber.get(confirmationNumber);
-	}
-
-	
-	public long book(Room room, Guest guest, Date arrivalDate, int stayLength, int occupantNumber,CreditCard creditCard) {           Booking booking = room.book(guest, arrivalDate, stayLength, occupantNumber, creditCard);
+        Booking booking = room.book(guest, arrivalDate, stayLength, occupantNumber, creditCard);
         long confirmationno = booking.getConfirmationNumber();
         bookingsByConfirmationNumber.put(confirmationno, booking);
           
-        return confirmationno;		
-	}
+        return confirmationno;
+    }
 
-	
-	public void checkin(long confirmationNumber,Room room, Guest guest, Date arrivalDate, int stayLength, int occupantNumber, CreditCard creditCard) 
-        {
+    public void checkin(long confirmationNumber,Room room, Guest guest, Date arrivalDate, int stayLength, int occupantNumber, CreditCard creditCard) 
+    {
        Booking booking = room.book(guest, arrivalDate, stayLength, occupantNumber, creditCard);
        
        tempConfNumber = booking.getConfirmationNumber();
@@ -105,21 +102,14 @@ public class Hotel {
        int roomId= booking.getRoomId();
        booking.checkIn();
        activeBookingsByRoomId.put(roomId,booking);
-	}
+    }
 
+    
 
-	public void addServiceCharge(int roomId, ServiceType serviceType, double cost) {
-	Booking booking = activeBookingsByRoomId.get(roomId);
-        if(booking==null)
-       {
-           throw new RuntimeException("No active booking associated with the room");
-       }
-         booking.addServiceCharge(serviceType,cost);
-	}
-
-	
-	public void checkout(int roomId) {
-		Booking booking = activeBookingsByRoomId.get(roomId);
+    public void checkout(int roomId) 
+    {
+      
+       Booking booking = activeBookingsByRoomId.get(roomId);
        
        if(booking==null)
        {
@@ -128,7 +118,17 @@ public class Hotel {
        
        booking.checkOut();
        activeBookingsByRoomId.remove(roomId);
-	}
-
+       
+    }
+    
+    public void addServiceCharge(int roomId, ServiceType serviceType, double cost) 
+    {
+        Booking booking = activeBookingsByRoomId.get(roomId);
+        if(booking==null)
+       {
+           throw new RuntimeException("No active booking associated with the room");
+       }
+         booking.addServiceCharge(serviceType,cost);
+    }
 
 }
